5a40a13aef30eec32ddf51656c62d2aa701d41c5
денис турков <turkov_d_v@mail.ru>
2022-09-14 23:25:40 +0300

    service first commit

---
+++ ServiceApp/Models/Manufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceApp.Models
+{
+    public partial class Manufacturer
+    {
+        public Manufacturer()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public DateTime? StartDate { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
